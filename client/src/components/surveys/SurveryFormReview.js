1e113caa0f9a6b8c10335e691860638677d24895
Domas Žičevičius <domas.zicevicius@gmail.com>
2019-02-09 19:40:13 +0200

    Posting to surveys api

--- client/src/components/surveys/SurveryFormReview.js
+++ client/src/components/surveys/SurveryFormReview.js
@@ -2,8 +2,10 @@ import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
+import { withRouter } from 'react-router-dom';
+import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
     const reviewFields = _.map(formFields, ({ name, label }) => {
         return (
             <div key={name}>
@@ -20,10 +22,19 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
             <h5>Please confirm your entries</h5>
             {reviewFields}
             <button
-                className="yellow darken-3 btn-flat"
+                className="teal white-text btn-flat"
+                style={{ marginTop: '20px'}}
                 onClick={onCancel}
             >
                 Back
+            </button>
+            <button 
+                onClick={() => submitSurvey(formValues, history)}
+                className="green btn-flat right white-text"
+                style={{ marginTop: '20px'}}
+            >
+                Send Survey
+                <i className="material-icons right">email</i>
             </button>
         </div>
     );
@@ -33,4 +44,4 @@ function mapStateToProps(state) {
     return { formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
--- client/src/components/surveys/formFields.js
+++ client/src/components/surveys/formFields.js
@@ -2,5 +2,5 @@ export default [
     { label: 'Survey Title', name: 'title' },
     { label: 'Subject Line', name: 'subject' },
     { label: 'Email Body', name: 'body' },
-    { label: 'RecipientList', name: 'emails' }
+    { label: 'RecipientList', name: 'recipients' }
 ];
